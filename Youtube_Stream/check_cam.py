d873c8e06303a359a76ea5eb8c4099668565812e
Hector Garcia <hectgt94@gmail.com>
2017-05-30 18:55:32 -0500

    act

--- Youtube_Stream/check_cam.py
+++ Youtube_Stream/check_cam.py
@@ -11,25 +11,21 @@ def check_pid(pid):
 
 print("empezemos")
 while True:
-	#try:
-	with open("log_Img.txt",'r') as file:
-		lines = file.readlines()
-		pid   = (lines[0]) 
-		time  = datetime.strptime(lines[1], '%Y-%m-%d %H:%M:%S')
+	try:
+		with open("log_Img.txt",'r') as file:
+			lines = file.readlines()
+			pid   = (lines[0]) 
+			time  = datetime.strptime(lines[1], '%Y-%m-%d %H:%M:%S')
 
-	now = datetime.now()
-	delta_t = now - time
-	tot_min = delta_t.total_seconds()/60
-	check = check_pid(int(pid))
+		now = datetime.now()
+		delta_t = now - time
+		tot_min = delta_t.total_seconds()/60
+		check = check_pid(int(pid))
 
-	print(pid)
-	print(time)
-	print(tot_min)
-
-	if (tot_min > 5) and (check == True):
-		print("vamo a matalo")
-		os.system("sudo kill " + pid)
-		os.system("sudo python /home/pi/EMUNPi/Youtube_Stream/Img_stream.py &")
-	#except Exception, e:
-	#	print(e)
-	#	pass
+		if (tot_min > 5) and (check == True):
+			print("vamo a matalo")
+			os.system("sudo kill " + pid)
+			os.system("sudo python /home/pi/EMUNPi/Youtube_Stream/Img_stream.py &")
+	except Exception, e:
+		print(e)
+		pass
