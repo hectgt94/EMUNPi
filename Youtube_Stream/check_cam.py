e136e2f1065363bff0c0cb69f1b26699a74ec879
Hector Garcia <hectgt94@gmail.com>
2017-05-30 18:17:31 -0500

    a

--- Youtube_Stream/check_cam.py
+++ Youtube_Stream/check_cam.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 import os
 
+def check_pid(pid):        
+    """ Check For the existence of a unix pid. """
+  try:
+    os.kill(pid, 0)
+  except OSError:
+    return False
+  else:
+    return True
+
 while True:
 	try:
 		with open("log_Img.txt",'r') as file:
@@ -11,8 +20,9 @@ while True:
 		now = datetime.now()
 		delta_t = now - time
 		tot_min = delta_t.total_seconds()/60
+		check = check_pid(pid)
 
-		if tot_min > 5:
+		if (tot_min > 5) and (check == True):
 			os.system("sudo kill " + pid)
 			os.system("sudo python /home/pi/EMUNPi/Youtube_Stream/Img_stream.py &")
 	except:
