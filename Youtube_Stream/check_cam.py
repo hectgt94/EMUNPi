68901a598a07a4693dd8e57e5de231678bb92a6f
Hector Garcia <hectgt94@gmail.com>
2017-05-30 18:03:28 -0500

    aa

--- Youtube_Stream/check_cam.py
+++ Youtube_Stream/check_cam.py
@@ -5,7 +5,7 @@ while True:
 	try:
 		with open("log_Img.txt",'r') as file:
 			lines = file.readlines()
-			pid   = int(lines[0]) 
+			pid   = (lines[0]) 
 			time  = datetime.strptime(lines[1], '%Y-%m-%d %H:%M:%S')
 
 		now = datetime.now()
@@ -13,7 +13,7 @@ while True:
 		tot_min = delta_t.total_seconds()/60
 
 		if tot_min > 1:
-			os.kill(pid, signal.SIGTERM)
+			os.system("sudo kill " + pid)
 			os.system("python /home/pi/EMUNPi/Youtube_Stream/Img_stream.py &")
 	except:
 		pass
